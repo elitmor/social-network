c3ed5120f18f67e781a947ba12c17e734977d3aa
elitmor <elitmor@gmail.com>
2023-05-09 18:41:44 +0300

    useParams

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,17 +1,23 @@
+import axios from 'axios';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { setUserProfileAC } from '../../redux/profile-reducer';
 import { MyPosts } from './myPosts/MyPosts';
 import style from './profile.module.css';
 import { ProfileInfo } from './profileInfo/ProfileInfo';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import { setUserProfileAC } from '../../redux/profile-reducer';
 
 export const Profile = (props: any) => {
   const dispatch = useDispatch();
+  let { userId } = useParams();
 
   useEffect(() => {
+    if (!userId) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      userId = '20178';
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/20178`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((res) => {
         dispatch(setUserProfileAC(res.data));
       });
