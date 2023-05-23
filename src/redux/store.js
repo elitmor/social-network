63490f51fa8213e07da44f3caca979b61af42bb8
elitmor <elitmor@gmail.com>
2023-05-23 11:46:10 +0300

    install redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import thunkMiddleWare from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { appReducer } from './app-reducer';
 import { authReducer } from './auth-reducer';
 import { dialogsReducer } from './dialogs-reducer';
@@ -14,6 +14,10 @@ const reducers = combineReducers({
   app: appReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleWare));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
 
 export default store;
