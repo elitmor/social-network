d95dc287777f623f5128a0816a957d0ce67a81fc
elitmor <elitmor@gmail.com>
2023-06-07 11:05:53 +0300

    typing MyDialogs

--- src/components/dialogs/MyDialogs.tsx
+++ src/components/dialogs/MyDialogs.tsx
@@ -1,12 +1,10 @@
-import { FC } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-
 import { getIsAuth } from '../../redux/auth-selectors';
 import { actions } from '../../redux/dialogs-reducer';
 import { getMessages } from '../../redux/dialogs-selectors';
-import { AppStateType } from '../../redux/store';
 import { Dialog } from './dialog/Dialog';
 import { Message } from './message/Message';
 import style from './myDialogs.module.css';
@@ -15,16 +13,16 @@ interface MyDialogsFormData {
   textarea: string;
 }
 
-const MyDialogs: FC = () => {
+const MyDialogs: React.FC = () => {
   const dispatch = useDispatch();
-  const messages = useSelector((state: AppStateType) => getMessages(state));
-  const isAuth = useSelector((state: AppStateType) => getIsAuth(state));
+  const messages = useSelector(getMessages);
+  const isAuth = useSelector(getIsAuth);
 
   const { register, handleSubmit, reset } = useForm<MyDialogsFormData>();
 
   const { addMessage } = actions;
 
-  const onSubmit: SubmitHandler<MyDialogsFormData> = (data) => {
+  const onSubmit = (data: MyDialogsFormData) => {
     dispatch(addMessage('Your Name', data.textarea));
     reset();
   };
